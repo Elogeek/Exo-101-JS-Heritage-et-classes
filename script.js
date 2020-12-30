10f4c61db14d022d5079973e91c2412823451e19
Elogeek <elodiechristin@gmail.com>
2020-12-30 15:27:30 +0100

    comit and puh et c'est fini

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 class Vehicule {
+
     constructor(couleur, roues, marque) {
         this.couleur = couleur;
         this.roues = roues;
         this.marque = marque;
     }
-        demarrer() {
-            console.log("je démarre");
-        }
-        arreter () {
-            console.log("je m'arrête");
-        }
+
+    start() {
+        console.log("je démarre");
     }
 
-    class Velo extends Vehicule {
-        constructor(couleur, roues, marque, rayonRoues, typePeinture) {
-            super(couleur, roues, marque);
-            this.rayonRoues = rayonRoues;
-            this.typePeinture = typePeinture;
-        }
-        monter() {
-            console.log("je monte sur mon vélo");
-        }
+    stop() {
+        console.log("je m'arrête");
+    }
+}
+
+class Velo extends Vehicule {
+
+    constructor(couleur, roues, marque, rayonRoues, typePeinture) {
+        super(couleur, roues, marque);
+        this.rayonRoues = rayonRoues;
+        this.typePeinture = typePeinture;
     }
 
-    class Voiture extends Vehicule {
+    monter() {
+        console.log("je monte sur mon vélo");
+    }
+}
+
+class Voiture extends Vehicule {
 
     constructor(couleur, roues, marque, assurance, proprietaire) {
         super(couleur, roues, marque);
         this.assurance = assurance;
         this.proprio = proprietaire;
     }
-        passerAuCarWash() {
-            console.log("Rouler toute propre c'est mieux !");
-        }
-    }
 
-    let Ford = new Voiture("turquoise", "4", "Ford", "ING+", "moi");
-    Ford.demarer();
+    /**
+     * la voiture doit passer au carWash!!!!
+     */
+    passerAuCarWash() {
+        console.log("Rouler toute propre c'est mieux !");
+    }
+}
+
+let Ford = new Voiture("turquoise", "4", "Ford", "AXA", "moi");
+Ford.start();
